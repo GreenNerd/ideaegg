741a82ad233b0ef7491075b89d1667e3cb04cd12
huangwei <676018683@qq.com>
2015-06-03 10:42:50 +0800

    refactor ideas controller test
    [Issue #7] https://github.com/GreenNerd/ideaegg/issues/7

--- spec/controllers/ideaegg_api/ideas_controller_spec.rb
+++ spec/controllers/ideaegg_api/ideas_controller_spec.rb
@@ -4,51 +4,53 @@ RSpec.describe IdeaeggApi::IdeasController, :type => :controller do
   include ApiSpecHelpers
   render_views
 
+  let!(:user) { create :user }
+
   before :each do
     request.env["HTTP_ACCEPT"] = 'application/json'
+    request.env["PRIVATE-TOKEN"] = user.private_token
   end
-
-  let!(:user) { create :user }
-  let(:token) { { private_token: user.private_token } }
 
   describe '#create' do
     let(:valid_attrs) { attributes_for :idea }
     let(:invalid_attrs) { attributes_for :idea, cover: nil }
+    let(:token) { { private_token: user.private_token } }
 
     context 'succeeding' do
       it 'creates a idea with token in params' do
+        request.env["PRIVATE-TOKEN"] = nil
         expect {
-          post :create, valid_attrs.merge(token)
+          post :create, valid_attrs.merge!(token)
         }.to change { Idea.count }.by 1
       end
 
       it 'creates a idea with token in header' do
-        @request.env['PRIVATE-TOKEN'] = user.private_token
         expect {
           post :create, valid_attrs
         }.to change { Idea.count }.by 1
       end
 
       it 'returns a idea json' do
-        post :create, valid_attrs.merge(token)
+        post :create, valid_attrs
         expect(json_response['content_html']).to eq "<p><strong>World</strong></p>\n"
       end
     end
 
     context 'validation failing' do
       it 'returns 422 code' do
-        post :create, invalid_attrs.merge(token)
+        post :create, invalid_attrs
         expect(response.status).to eq 422
       end
 
       it 'returns errors json' do
-        post :create, invalid_attrs.merge(token)
+        post :create, invalid_attrs
         expect(json_response['errors']).not_to be_nil
       end
     end
 
     context 'authentication failing' do
       it 'returns 401 code' do
+        request.env["PRIVATE-TOKEN"] = nil
         post :create, valid_attrs
         expect(response.status).to eq 401
       end
@@ -59,12 +61,12 @@ RSpec.describe IdeaeggApi::IdeasController, :type => :controller do
     let!(:idea) { create :idea }
 
     it 'returns the idea json' do
-      get :show, { id: idea }.merge(token)
+      get :show, id: idea
       expect(json_response['id']).to eq idea.id
     end
 
     it 'returns 404 code if idea is not found' do
-      get :show, { id: -1 }.merge(token)
+      get :show, id: -1
       expect(response.status).to eq 404
     end
   end
@@ -74,17 +76,17 @@ RSpec.describe IdeaeggApi::IdeasController, :type => :controller do
     let!(:another_idea) { create :idea }
 
     it 'assigns the ideas' do
-      get :index, { per_page: 1, page: 1 }.merge(token)
+      get :index, { per_page: 1, page: 1 }
       expect(assigns(:ideas)).to eq [another_idea]
     end
 
     it 'returns a ideas json' do
-      get :index, { per_page: 1, page: 1 }.merge(token)
+      get :index, { per_page: 1, page: 1 }
       expect(json_response.first['id']).to eq another_idea.id
     end
 
     it 'response link header includes pagination info' do
-      get :index, { per_page: 1, page: 1 }.merge(token)
+      get :index, { per_page: 1, page: 1 }
       expect(response.header['Link']).not_to be_nil
     end
   end
@@ -93,19 +95,20 @@ RSpec.describe IdeaeggApi::IdeasController, :type => :controller do
     let!(:idea) { create :idea }
 
     it 'let user like the idea' do
-      put :vote, { id: idea.id }.merge(token)
+      put :vote, { id: idea.id }
       expect(user.liked? idea).to be_truthy
     end
   end
 
   describe 'Delete unvote' do
     let!(:idea) { create :idea }
+
     before :each do
       user.likes idea
     end
 
     it 'let user unlike the idea' do
-      delete :unvote, { id: idea.id }.merge(token)
+      delete :unvote, { id: idea.id }
       expect(user.liked? idea).to be_falsey
     end
   end
@@ -114,30 +117,29 @@ RSpec.describe IdeaeggApi::IdeasController, :type => :controller do
     let!(:idea) { create :idea }
 
     it 'let user star the idea' do
-      put :star, { id: idea.id }.merge(token)
+      put :star, { id: idea.id }
       expect(idea.starred_by? user).to be_truthy
     end
   end
 
   describe 'DELETE star' do
     let!(:idea) { create :idea }
+
     before :each do
       idea.starred_by! user
     end
 
     it 'unstars the idea' do
-      delete :unstar, { id: idea.id }.merge(token)
+      delete :unstar, { id: idea.id }
       expect(idea.starred_by? user).to be_falsey
     end
   end
 
   describe 'GET voted' do
-    let(:user) { create :user }
     let(:idea) { create :idea }
     let(:another_idea) { create :idea }
 
     before :each do
-      @request.env['PRIVATE-TOKEN'] = user.private_token
       user.likes idea
       user.likes another_idea
     end
@@ -155,12 +157,10 @@ RSpec.describe IdeaeggApi::IdeasController, :type => :controller do
   end
 
   describe 'GET starred' do
-    let(:user) { create :user }
     let(:idea) { create :idea }
     let(:another_idea) { create :idea }
 
     before :each do
-      request.env["PRIVATE-TOKEN"] = user.private_token
       idea.starred_by! user
       another_idea.starred_by! user
     end
@@ -178,12 +178,7 @@ RSpec.describe IdeaeggApi::IdeasController, :type => :controller do
   end
 
   describe 'GET created' do
-    let(:user) { create :user }
     let!(:idea) { create :idea, user_id: user.id }
-
-    before :each do
-      request.env["PRIVATE-TOKEN"] = user.private_token
-    end
 
     it 'assigns the ideas' do
       get :created
@@ -198,13 +193,8 @@ RSpec.describe IdeaeggApi::IdeasController, :type => :controller do
   end
 
   describe 'POST tags' do
-    let(:user) { create :user }
     let!(:idea) { create :idea, user_id: user.id }
     let(:tag_params) { { tag: 'name1, name2' } }
-
-    before :each do
-      request.env["PRIVATE-TOKEN"] = user.private_token
-    end
 
     it 'add two tags for the idea' do
       expect {
